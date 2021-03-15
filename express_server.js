39483da22868025591c9f3f2b564b88589c3b0f5
Martin Halas <echoes.time@gmail.com>
2021-03-15 21:55:47 +0000

    Added random string generating function to make URLs. Also added post route for new URLs

--- express_server.js
+++ express_server.js
@@ -2,11 +2,23 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
+};
+
+const generateRandomString = () => {
+  const charArr = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" // length is 62
+  let randString = '';
+  for (let i = 0; i < 6; i++) {
+    randString += charArr[(Math.floor(Math.random() * 62))]; // adds random letter from charArr
+  }
+  return randString;
 };
 
 app.get("/", (req, res) => {
@@ -17,6 +29,11 @@ app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("Ok");
+})
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
