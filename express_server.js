ae27bd7d086a5583d0b4220651715285bc68da3a
Martin Halas <echoes.time@gmail.com>
2021-03-16 22:47:11 +0000

    Added login and logout form using cookies

--- express_server.js
+++ express_server.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const cookieParser = require('cookie-parser');
+
 const app = express();
 const PORT = 8080; // default port 8080
 
-const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 // app.use(express.urlencoded()); // better version of bodyparser?? get more info
+app.use(cookieParser());
 
 app.set("view engine", "ejs");
 
@@ -31,7 +34,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("/urls", (req, res) => { // homepage/url-list
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { urls: urlDatabase, username: req.cookies["username"]};
   res.render("urls_index", templateVars);
 });
 
@@ -40,11 +43,10 @@ app.post("/urls", (req, res) => { // user sends request to generate shortened ur
   if (Object.values(urlDatabase).indexOf((longURL)) > -1) {
     const shortURL = getKeyByValue(longURL);
     res.redirect(`/urls/${shortURL}`);
-  } else {
-    const shortURL = generateRandomString();
-    urlDatabase[shortURL] = longURL;
-    res.redirect(`/urls/${shortURL}`);
   }
+  const shortURL = generateRandomString();
+  urlDatabase[shortURL] = longURL;
+  res.redirect(`/urls/${shortURL}`);
 });
 
 app.post("/urls/:shortURL/delete", (req, res) => {
@@ -58,11 +60,12 @@ app.get("/u/:shortURL", (req, res) => {
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = { username: req.cookies["username"] };
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username: req.cookies["username"] };
   res.render("urls_show", templateVars);
 });
 
@@ -83,6 +86,18 @@ app.post("/urls/:shortURL", (req, res) => {
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
+
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  res.cookie('username', username);
+  res.redirect('/urls');
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie('username');
+  res.redirect('/urls');
+})
+
 
 app.listen(PORT, () => {
   console.log(`TinyApp server listening on port ${PORT}!`);
